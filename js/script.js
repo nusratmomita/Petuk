36c43ce4971caefdd453243fff8a7b9ebe84dc2c
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2026-02-13 21:19:38 +0600

    Header section designing

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".offer_swiper", {
+  loop: false,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+    clickable: true
+  },
+});
